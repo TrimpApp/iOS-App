6fe67f6770f68eb477e348270b5ed1dbc4abb115
975 Productions <ddeltathemediaguy@gmail.com>
2024-08-03 15:42:53 -0300

    initial commit

---
+++ TrimpTests/TrimpTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrimpTests.swift
+//  TrimpTests
+//
+//  Created by Silverstar A. Carlson on 03/08/2024.
+//
+
+import Testing
+@testable import Trimp
+
+struct TrimpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
